7281ad73d6dff1263d4f992d563456a1227d7c6c
Undram <mariyakylie18@gmail.com>
2026-09-03 15:49:29 +0700

    Prepare backend for deployment

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const protect = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-    console.log("AUTH HEADER:", authHeader);
 
     if (!authHeader) {
       return res.status(401).json({
@@ -17,13 +16,13 @@ const protect = (req, res, next) => {
       });
     }
 
-    const decoded = jwt.verify(token, "mySecretKey");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
 
     next();
   } catch (error) {
     return res.status(401).json({
-      message: "Invalid or e4xpried token",
+      message: "Invalid or expired token",
     });
   }
 };
